4e6c4ab7ad19479447cc2c0d1283185c078e15a3
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2024-11-22 14:49:52 +0100

    installed ShadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+// Node modules
+import { Button } from "@/components/ui/button";
+
+// Project files
+import "./styles/style.css";
+
 export default function App() {
   return (
     <div className="app">
       <nav>Sidebar</nav>
       <main>Page</main>
+      <Button>Click me</Button>
     </div>
   );
 }
